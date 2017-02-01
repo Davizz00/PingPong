32dadf3b6d474a79bad89189ded231ab9f04730b
David Hernández puerta <David Hernndez puerta>
2017-02-02 00:29:42 +0100

    canvas completo

--- Ping_Pong/PingPong.js
+++ Ping_Pong/PingPong.js
@@ -1,25 +1,51 @@
+var canvas = document.getElementById("myCanvas");
+var widht = 25;
+var height = 100;
+
+
+
 function jugador1(){
     var canvas = document.getElementById("myCanvas");
-    var ctx = canvas.getContext("2d");
-    ctx.fillStyle = "green";
-    ctx.fillRect(100, 250, 20, 100);
+	var pala1 = canvas.getContext("2d");
+	pala1.fillStyle = "green";
+	pala1.fillRect(100,250-(height/2),widht,height)
+	console.log("Hola");
+    function evento(){
+        window.setInterval(move,1000);
+        //document.addEventListener("keypress", move);
+        function move(){
+            
+            pala1.fillrect = (100,250-(height/2)+100,widht,height);
+            console.log("move");
+            
+        }
+    move()    
+            
+    } 
+    evento();
+    
 }
 function jugador2(){
     var canvas = document.getElementById("myCanvas");
     var ctx = canvas.getContext("2d");
-    ctx.fillStyle = "green";
-    ctx.fillRect(900, 250, 20, 100);
+    ctx.fillStyle = "orange";
+    ctx.fillRect(900 - widht,250-(height/2), widht, height);
+	console.log("adios");
 }
 
 function pelota(){
     var canvas = document.getElementById("myCanvas");
     var ctx = canvas.getContext("2d");
     ctx.fillStyle = "blue";
-    ctx.fillRect(495, 245, 10, 10);
+    ctx.fillRect(500-(widht/2), 250-(widht/2), widht, widht);
+	console.log("tsss");
 }
 function initcanvas(){
-    window.onload = jugador1;
-    window.onload = jugador2;
-    window.onload = pelota;
-    
-}
+    jugador1();
+    jugador2();
+    pelota();
+	console.log("dfdffsf");
+   
+ }
+ 
+   
